d81b089bbdd0a8d0de45bb8b19bde951a60352f4
72312150 <72312150@proton.me>
2024-01-24 08:17:48 -0800

    added functions for counting words and letters

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world")
+def main():
+  path = "books/frankenstein.txt"
+  text = get_book_text(path)
+  letters = letter_count(text)
+  print(f"{letters}")
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+
+def word_count(text):
+  words = text.split()
+  return len(words)
+  
+def letter_count(text):
+  letter_count_dict = {}
+  text = text.lower() # This assigns the .lower result back to text.
+  for char in text:
+    if char not in letter_count_dict:
+      letter_count_dict[char] = 1 # if the character isnt already in the dict, add it and set its count to 1.
+    else:
+      letter_count_dict[char] += 1 # if the character is already in the dict, increase its count by 1.
+  return letter_count_dict
+
+if __name__ == "__main__":
+  main()
